15c9c33580302a78eb643573c2363b0cf09c5786
Bobovino <rodrigomartineztabernero97@gmail.com>
2023-06-26 16:46:55 +0200

    Fixing bugs and adding some styles

--- React/draftsimulator/src/Components/DroppableCell.js
+++ React/draftsimulator/src/Components/DroppableCell.js
@@ -25,19 +25,29 @@ function DroppableCell({ champion, setChampion, selectedChampions, setSelectedCh
     accept: 'champion',
     drop: (droppedItem, monitor) => {
       const { champion: droppedChampion } = droppedItem;
-  
+    
       // If there's already a champion in the cell, swap it with the dropped champion
       if (champion) {
         const originalChampion = champion;
         setChampion(droppedChampion);
-        setSelectedChampions(prev => new Set([...prev, droppedChampion.name]));
+        setSelectedChampions((prev) => {
+          const newSet = new Set(prev);
+          // If the dropped champion is not already in a DroppableCell (i.e., it's not in the set),
+          // remove the original champion from the set
+          if (!prev.has(droppedChampion.name)) {
+            newSet.delete(originalChampion.name);
+          }
+          newSet.add(droppedChampion.name); // Add the dropped champion to the set
+          return newSet;
+        });
         return { champion: originalChampion };
       }
-  
+    
       // Otherwise, just set the dropped champion
       setChampion(droppedChampion);
-      setSelectedChampions(prev => new Set([...prev, droppedChampion.name]));
+      setSelectedChampions((prev) => new Set(prev).add(droppedChampion.name));
     },
+    
     collect: (monitor) => ({
       isOver: monitor.isOver(),
     }),
@@ -51,6 +61,7 @@ function DroppableCell({ champion, setChampion, selectedChampions, setSelectedCh
     display: 'flex',
     justifyContent: 'center',
     backgroundColor: isOver ? 'lightblue' : 'white',
+    
   };
 
   const cellRef = useRef(null);
@@ -63,13 +74,13 @@ function DroppableCell({ champion, setChampion, selectedChampions, setSelectedCh
     style={cellStyle}
     title="Drag and drop to switch positions. Right click to cancel the selection"
     onContextMenu={(e) => {
-      e.preventDefault();
+      e.preventDefault(); // Prevent the context menu from showing
       if (champion) {
       setSelectedChampions(prev => {
         const newSelectedChampions = new Set([...prev]);
         newSelectedChampions.delete(champion.name);
         return newSelectedChampions;
-      }); // Prevent the context menu from showing
+      }); 
       setChampion(null); // Make the cell empty
     }}
     }
